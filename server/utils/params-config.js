121a3debe0fea59a43eeabe5513bbbe003bb1421
Niles87 <nbing19@gmail.com>
2021-05-27 21:39:13 -0700

    added route on backend for image uploads

---
+++ server/utils/params-config.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: process.env.BUCKET,
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+
+  return imageParams;
+};
+
+module.exports = params;
